c412edb3a865b52a32be7f909e04e602fda2bd72
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-18 18:02:56 -0300

    corrige file server.ts with eslint

--- server.ts
+++ server.ts
@@ -3,6 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(3200, () => {
-    console.log("Server listening on port 3200")
+  console.log("Server listening on port 3200");
 });
-
