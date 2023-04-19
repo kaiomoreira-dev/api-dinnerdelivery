6ff6a06c3789deed2ac48d469b731f99e0de515f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-19 16:26:05 -0300

    create interface for methods for repository UserRepository

---
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -0,0 +1,16 @@
+import { ICreateUserDTO } from "../dtos/CreateUserDTO";
+import { Users } from "../infra/typeorm/entities/Users";
+
+export interface IUsersRepository {
+    create(data: ICreateUserDTO): Promise<Users>;
+    findById(id: string): Promise<Users>;
+    findByEmail(email: string): Promise<Users>;
+    updateById(
+        id: string,
+        name: string,
+        addres: string,
+        email: string,
+        password: string
+    ): Promise<void>;
+    deleteById(id: string): Promise<void>;
+}
