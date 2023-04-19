f5b20155e5840cbba1cfd5021b30b04df9fd0290
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-19 16:26:29 -0300

    create controller users

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController {
+    async hundle(request: Request, response: Response): Promise<Response> {
+        const { name, password, email, address } = request.body;
+
+        const createUserUseCase = container.resolve(CreateUserUseCase);
+
+        await createUserUseCase.execute({
+            name,
+            password,
+            email,
+            address,
+        });
+
+        return response.status(201).json();
+    }
+}
