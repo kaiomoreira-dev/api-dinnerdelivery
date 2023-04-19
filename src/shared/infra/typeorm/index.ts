8b6710e1a0269970fe9554164db5bab64e1f308d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-19 13:08:28 -0300

    create configuration for connection postrges

---
+++ src/shared/infra/typeorm/index.ts
@@ -0,0 +1,23 @@
+import { DataSource } from "typeorm";
+
+import { CreateUsers1681917942698 } from "./migrations/1681917942698-CreateUsers";
+
+const dataSource = new DataSource({
+  type: "postgres",
+  port: 1999,
+  username: "docker",
+  password: "YWfv44186uG6oBetxJC7sOAxJttd6aLe0zpmb_xdwpQ",
+  database:
+    process.env.NODE_ENV === "test" ? "dinnerDelivery_test" : "dinnerDelivery",
+
+  // importar entidades ex: [Recipes]
+  entities: [],
+  // importar migrations ex: [CreateRecipes102348998]
+  migrations: [CreateUsers1681917942698],
+});
+
+export function createConnection(host = "localhost"): Promise<DataSource> {
+  return dataSource.setOptions({ host }).initialize();
+}
+
+export default dataSource;
