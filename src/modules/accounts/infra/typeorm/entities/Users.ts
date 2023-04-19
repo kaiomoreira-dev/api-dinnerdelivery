9fcba7f5e396eb15d9a344ea8241b093d9a627fb
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-19 16:24:22 -0300

    create entity for User

---
+++ src/modules/accounts/infra/typeorm/entities/Users.ts
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryColumn,
+    UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+@Entity("users")
+export class Users {
+    @PrimaryColumn()
+    id?: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    isAdmin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4();
+        }
+    }
+}
