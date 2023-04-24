55d4b246c4292e9e1fcef019aa7ea754c0105b8e
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-24 19:52:08 -0300

    create auth file for configuration token expire_in and secret

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: process.env.SECRET_TOKEN,
+  expire_in_token: process.env.EXPIRE_IN_TOKEN,
+  //   secret_refresh_token: "4537f8e0a1134a4722aeaa326a05446964ca56d0",
+  //   expire_in_refresh_token: "30d",
+  //   expire_days_refresh_token: 30,
+};
