51955bcba6931def466ba6bd75efa791efc2a791
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-24 19:51:38 -0300

    add route autheticate for index file

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { usersRoutes } from "./users.routes";
 
 export const router = Router();
 
 router.use("/users", usersRoutes);
+
+router.use(authenticateRoutes);
