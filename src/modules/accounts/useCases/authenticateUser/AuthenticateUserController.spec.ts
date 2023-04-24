e058d409cdf7ac75fa0704c0c4bcb45310830479
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-24 20:01:07 -0300

    create test for valid authenticate user, verify password nad email

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -0,0 +1,78 @@
+import "dotenv/config";
+import { faker } from "@faker-js/faker";
+import { ICreateUserDTO } from "@modules/accounts/dtos/CreateUserDTO";
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import { createConnection } from "@shared/infra/typeorm";
+
+let connection: DataSource;
+
+describe("Authenticate User Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection("localhost");
+
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+
+        await connection.destroy();
+    });
+
+    it("should be able to authenticate a user", async () => {
+        const user: ICreateUserDTO = {
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(10),
+            address: faker.address.streetAddress(),
+        };
+        await request(app).post("/users").send(user);
+
+        const authenticateUser = await request(app).post("/sessions").send({
+            email: user.email,
+            password: user.password,
+        });
+
+        expect(authenticateUser.status).toBe(200);
+    });
+
+    it("should not be able to authenticate a user with a wrong password", async () => {
+        const user: ICreateUserDTO = {
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(10),
+            address: faker.address.streetAddress(),
+        };
+        await request(app).post("/users").send(user);
+
+        const authenticateUser = await request(app).post("/sessions").send({
+            email: user.email,
+            password: "fakepass",
+        });
+
+        expect(authenticateUser.status).toBe(401);
+    });
+
+    it("should not be able to authenticate a user with a wrong email", async () => {
+        const user: ICreateUserDTO = {
+            id: faker.datatype.uuid(),
+            name: faker.name.fullName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(10),
+            address: faker.address.streetAddress(),
+        };
+        await request(app).post("/users").send(user);
+
+        const authenticateUser = await request(app).post("/sessions").send({
+            email: "fakeEmail",
+            password: user.password,
+        });
+
+        expect(authenticateUser.status).toBe(401);
+    });
+});
