b0189c540a1b231c3b23869b44907b43b8a3d5a0
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-25 17:51:35 -0300

    create route for refresh-token user

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,8 +1,13 @@
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 import { Router } from "express";
 
 export const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 
+const refreshTokenController = new RefreshTokenController();
+
 authenticateRoutes.post("/sessions", authenticateUserController.hundle);
+
+authenticateRoutes.post("/refresh-token", refreshTokenController.hundle);
