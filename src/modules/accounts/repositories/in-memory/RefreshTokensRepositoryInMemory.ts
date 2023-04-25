5a547a19a0ea9f3d528a00cf6429844bc1c817ea
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-25 16:01:19 -0300

    create repository refresh tokens in memory

---
+++ src/modules/accounts/repositories/in-memory/RefreshTokensRepositoryInMemory.ts
@@ -0,0 +1,47 @@
+import { IRefreshTokensDTO } from "@modules/accounts/dtos/IRefreshTokensDTO";
+import { RefreshTokens } from "@modules/accounts/infra/typeorm/entities/RefreshTokens";
+
+import { IRefreshTokensRepository } from "../IRefreshTokensRepository";
+
+export class RefreshTokensRepositoryInMemory
+    implements IRefreshTokensRepository
+{
+    private usersTokens: RefreshTokens[] = [];
+
+    async create({
+        refresh_token,
+        expire_date,
+        id_users,
+    }: IRefreshTokensDTO): Promise<RefreshTokens> {
+        const refreshToken = new RefreshTokens();
+
+        Object.assign(refreshToken, {
+            refresh_token,
+            expire_date,
+            id_users,
+        });
+
+        this.usersTokens.push(refreshToken);
+
+        return refreshToken;
+    }
+    async findRefreshTokenByUserIdAndRefreshToken(
+        id_users: string,
+        refresh_token: string
+    ): Promise<RefreshTokens> {
+        const refreshToken = this.usersTokens.find(
+            (token) =>
+                token.id_users === id_users &&
+                token.refresh_token === refresh_token
+        );
+
+        return refreshToken;
+    }
+    async deleteById(id: string): Promise<void> {
+        const refreshToken = this.usersTokens.find(
+            (token) => token.id_users === id
+        );
+
+        this.usersTokens.splice(this.usersTokens.indexOf(refreshToken));
+    }
+}
