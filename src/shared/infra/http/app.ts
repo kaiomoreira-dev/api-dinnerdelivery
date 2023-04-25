60192923ccf3ffa7a24f6fa9b804c0d287efb85f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-25 15:57:25 -0300

    configure cors for receive requisitions put, delete, get and more...

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -22,13 +22,16 @@ export const app = express();
 
 app.use(express.json());
 
-app.use(router);
-
 app.use(
   cors({
     origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   })
 );
+
+app.use(router);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
