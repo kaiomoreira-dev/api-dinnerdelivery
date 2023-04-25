ba4c6502bc68f8c788208d1ecf93ad374e4d1c2a
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-25 15:50:40 -0300

    create vars for tokens propritiers expire, days

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 export default {
   secret_token: process.env.SECRET_TOKEN,
   expire_in_token: process.env.EXPIRE_IN_TOKEN,
-  //   secret_refresh_token: "4537f8e0a1134a4722aeaa326a05446964ca56d0",
-  //   expire_in_refresh_token: "30d",
-  //   expire_days_refresh_token: 30,
+  secret_refresh_token: process.env.SECRET_REFRESH_TOKEN,
+  expire_refresh_token: process.env.EXPIRE_REFRESH_TOKEN,
+  days_refresh_token: Number(process.env.DAYS_REFRESH_TOKEN),
 };
