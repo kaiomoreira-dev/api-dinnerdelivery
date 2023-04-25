86ee3166827ea05eed386e1cc990e0eb7c3a652b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-25 15:56:49 -0300

    create container for refresh tokens repository and import providers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { RefreshTokensRepository } from "@modules/accounts/infra/typeorm/repositories/RefreshTokensRepository";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { IRefreshTokensRepository } from "@modules/accounts/repositories/IRefreshTokensRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { container } from "tsyringe";
+
+import "./providers";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+);
+
+container.registerSingleton<IRefreshTokensRepository>(
+  "RefreshTokensRepository",
+  RefreshTokensRepository
 );
