192ac2ef2faecfff7525e6bb68fdb2996084502d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-26 14:48:13 -0300

    create middlewares for authenticate user

---
+++ src/shared/infra/http/middlewares/ensureAuthenticate.ts
@@ -0,0 +1,40 @@
+import auth from "@config/auth";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticate(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  // destruturar do headers o toke
+  const authHeader = request.headers.authorization;
+
+  // validar no if pra ve se existe
+  if (!authHeader) {
+    throw new AppError("Miss token", 401);
+  }
+  // destruturar o token de dentro do authHeader
+  const [, token] = authHeader.split(" ");
+  // verificar no verify o token
+  // retirar de dentro do verify o id do user que esta no token
+  try {
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
+
+    // depois pesquisar em um método findbyid que vamos criar
+
+    // adicionar user_id no request do express
+    request.user = {
+      id: user_id,
+    };
+    next();
+  } catch {
+    throw new AppError("Invalid token", 401);
+  }
+}
