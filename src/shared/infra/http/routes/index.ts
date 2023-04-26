d82f8d18925f6d9f78b36603907afe5e46c13d96
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-04-26 14:49:15 -0300

    add route order in index routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
+import { orderRoutes } from "./orders.routes";
 import { usersRoutes } from "./users.routes";
 
 export const router = Router();
 
 router.use("/users", usersRoutes);
+router.use("/orders", orderRoutes);
 
 router.use(authenticateRoutes);
