3198f89c322e4e088a2e64a9844e4feeabb5cddb
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-01 15:45:51 -0300

    alter index adding entitie and migration for products, productsShoppingCarts and shoppingCarts

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,9 +1,15 @@
 import { RefreshTokens } from "@modules/accounts/infra/typeorm/entities/RefreshTokens";
 import { Users } from "@modules/accounts/infra/typeorm/entities/Users";
+import { Products } from "@modules/products/infra/typeorm/entities/Products";
+import { ProductsShoppingCarts } from "@modules/shoppingCarts/infra/typeorm/entities/ProductsShoppingCarts";
+import { ShoppingCarts } from "@modules/shoppingCarts/infra/typeorm/entities/ShoppingCarts";
 import { DataSource } from "typeorm";
 
 import { CreateUsers1681917942698 } from "./migrations/1681917942698-CreateUsers";
 import { CreateRefreshToken1682435280583 } from "./migrations/1682435280583-CreateRefreshToken";
+import { CreateProdutcs1682627046289 } from "./migrations/1682627046289-CreateProdutcs";
+import { CreateShoppingCart1682685180882 } from "./migrations/1682685180882-CreateShoppingCart";
+import { CreateProductsShoppingCarts1682689664277 } from "./migrations/1682689664277-CreateProductsShoppingCarts";
 
 const dataSource = new DataSource({
   type: "postgres",
@@ -14,9 +20,21 @@ const dataSource = new DataSource({
     process.env.NODE_ENV === "test" ? "dinnerdelivery_test" : "dinnerDelivery",
 
   // importar entidades ex: [Recipes]
-  entities: [Users, RefreshTokens],
+  entities: [
+    Users,
+    RefreshTokens,
+    Products,
+    ShoppingCarts,
+    ProductsShoppingCarts,
+  ],
   // importar migrations ex: [CreateRecipes102348998]
-  migrations: [CreateUsers1681917942698, CreateRefreshToken1682435280583],
+  migrations: [
+    CreateUsers1681917942698,
+    CreateRefreshToken1682435280583,
+    CreateProdutcs1682627046289,
+    CreateShoppingCart1682685180882,
+    CreateProductsShoppingCarts1682689664277,
+  ],
 });
 
 export function createConnection(host = "localhost"): Promise<DataSource> {
