3edb5c33fcde37d55849d67a5bcda01d0eba4c93
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-01 15:43:32 -0300

    create route products and shoppingCart

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
 import { orderRoutes } from "./orders.routes";
+import { productRoutes } from "./products.routes";
+import { shoppingCartRoutes } from "./shoppingCarts.routes";
 import { usersRoutes } from "./users.routes";
 
 export const router = Router();
 
 router.use("/users", usersRoutes);
 router.use("/orders", orderRoutes);
+router.use("/products", productRoutes);
+router.use("/shoppingCart", shoppingCartRoutes);
 
 router.use(authenticateRoutes);
