0b1a1c21170f8aeb60cc586c2db8d2b6eed27af9
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-01 16:10:18 -0300

    create controller for ProductsShoppingCarts

---
+++ src/modules/shoppingCarts/useCases/addProductInCart/CreateProductsShoppingCartsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateProductsShoppingCartsUseCase } from "./CreateProductsShoppingCartsUseCase";
+
+export class CreateProductsShoppingCartsController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { idProducts, quantity, idShoppingCarts } = request.params;
+
+        const quantityFormat = Number(quantity);
+        const createProductsShoppingCartsUseCase = container.resolve(
+            CreateProductsShoppingCartsUseCase
+        );
+
+        const addProductInSoppingCart =
+            await createProductsShoppingCartsUseCase.execute({
+                id_products: idProducts,
+                id_shoppingCarts: idShoppingCarts,
+                quantity: quantityFormat,
+            });
+
+        return response.status(200).json(addProductInSoppingCart);
+    }
+}
