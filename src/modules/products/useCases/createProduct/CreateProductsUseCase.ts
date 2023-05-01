77429ebf39e9da659c2a043d7d0d58545890cf72
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-01 15:58:36 -0300

    create use case file for validation products when for create

---
+++ src/modules/products/useCases/createProduct/CreateProductsUseCase.ts
@@ -0,0 +1,56 @@
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ICreateProductsDTO } from "@modules/products/dtos/ICreateProductsDTO";
+import { Products } from "@modules/products/infra/typeorm/entities/Products";
+import { IProductsRepository } from "@modules/products/repositories/IProductsRepository";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+export class CreateProductsUseCase {
+    constructor(
+        @inject("ProductsRepository")
+        private productsRepository: IProductsRepository,
+        @inject("UsersRepository")
+        private userRepository: IUsersRepository
+    ) {}
+
+    async execute(
+        { id, name, description, quantity, unit_price }: ICreateProductsDTO,
+        id_users: string
+    ): Promise<Products> {
+        const userAuthorized = await this.userRepository.findById(id_users);
+
+        if (!userAuthorized) {
+            throw new AppError("User not authorized to create products", 401);
+        }
+
+        const productsExists = await this.productsRepository.findByName(name);
+
+        if (productsExists) {
+            throw new AppError("Product already exists", 401);
+        }
+
+        if (quantity <= 0 || !quantity) {
+            throw new AppError("Quantity is not valid", 401);
+        }
+
+        if (unit_price <= 0 || !unit_price) {
+            throw new AppError("Price unit is not valid", 401);
+        }
+
+        if (description.length < 6 || !description) {
+            throw new AppError("Description is not valid", 401);
+        }
+
+        const product = await this.productsRepository.create({
+            id,
+            name,
+            description,
+            quantity,
+            unit_price,
+        });
+
+        return product;
+    }
+}
