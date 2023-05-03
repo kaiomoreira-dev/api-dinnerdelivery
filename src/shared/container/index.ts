a24d9aba54fd6c8a11a4bbf04cea613eb2ae5584
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-03 16:09:52 -0300

    import conatiner for repository in memory

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,16 +2,17 @@
 import { RefreshTokensRepository } from "@modules/accounts/infra/typeorm/repositories/RefreshTokensRepository";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IRefreshTokensRepository } from "@modules/accounts/repositories/IRefreshTokensRepository";
-import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 import "./providers";
 import { ProductsRepository } from "@modules/products/infra/typeorm/repositories/ProductsRepository";
 import { IProductsRepository } from "@modules/products/repositories/IProductsRepository";
 import { ProductsShoppingCartsRepository } from "@modules/shoppingCarts/infra/typeorm/repositories/ProductsShoppingCartsRepository";
 import { ShoppingCartsRepository } from "@modules/shoppingCarts/infra/typeorm/repositories/ShoppingCartsRepository";
-import { IShoppingCartsRepository } from "@modules/shoppingCarts/repositories/IShoppingCartsRepository";
 import { IProductsShoppingCartsRepository } from "@modules/shoppingCarts/repositories/IProductsShoppingCartsRepository";
+import { IShoppingCartsRepository } from "@modules/shoppingCarts/repositories/IShoppingCartsRepository";
 import { container } from "tsyringe";
+import "@shared/container/in-memory";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
