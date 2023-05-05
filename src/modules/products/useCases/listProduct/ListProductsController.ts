dbb7038a131c5d6b392bdc623091baf749ce3fa1
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-05 16:14:35 -0300

    create use controller for list all products

---
+++ src/modules/products/useCases/listProduct/ListProductsController.ts
@@ -0,0 +1,15 @@
+import { CreateOrderUseCase } from "@modules/orders/useCases/createOrder/CreateOrderUseCase";
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { ListProductsUseCase } from "./ListProductsUseCase";
+
+export class ListProductsController {
+    async handle(request: Request, response: Response) {
+        const listProductsUseCase = container.resolve(ListProductsUseCase);
+
+        const products = await listProductsUseCase.execute();
+
+        return response.status(200).json(products);
+    }
+}
