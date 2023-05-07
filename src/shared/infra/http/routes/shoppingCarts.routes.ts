2ab822884ab6f25365d2dc94aef7adef77c8af1d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-07 00:44:09 -0300

    create route for update shoppingcart

--- src/shared/infra/http/routes/shoppingCarts.routes.ts
+++ src/shared/infra/http/routes/shoppingCarts.routes.ts
@@ -1,5 +1,6 @@
 import { CreateProductsShoppingCartsController } from "@modules/shoppingCarts/useCases/addProductInCart/CreateProductsShoppingCartsController";
 import { FindShoppingCartByIdController } from "@modules/shoppingCarts/useCases/findShoppingCartById/FindShoppingCartByIdController";
+import { UpdateShoppingCartController } from "@modules/shoppingCarts/useCases/updateShoppingCart/UpdateShoppingCartController";
 import { Router } from "express";
 
 export const shoppingCartRoutes = Router();
@@ -9,9 +10,16 @@ const createProductsShoppingCartsController =
 
 const findShoppingCartByIdController = new FindShoppingCartByIdController();
 
+const updateShoppingCartController = new UpdateShoppingCartController();
+
 shoppingCartRoutes.post(
   "/add-product/:idProducts/:quantity/:idShoppingCarts?",
   createProductsShoppingCartsController.handle
 );
 
 shoppingCartRoutes.get("/:id", findShoppingCartByIdController.handle);
+
+shoppingCartRoutes.patch(
+  "/:id_products/:quantity/:id_shoppingCarts",
+  updateShoppingCartController.handle
+);
