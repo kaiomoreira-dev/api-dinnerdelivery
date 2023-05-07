70290e4b5953f7f684fd014c9cfa2086bb4d962b
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-07 00:45:04 -0300

    create controller for update shopping cart

---
+++ src/modules/shoppingCarts/useCases/updateShoppingCart/UpdateShoppingCartController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UpdateShoppingCartUseCase } from "./UpdateShoppingCartUseCase";
+
+export class UpdateShoppingCartController {
+    async handle(request: Request, response: Response) {
+        const { id_products, id_shoppingCarts, quantity } = request.params;
+
+        const qunatityNumber = Number(quantity);
+
+        const updateShoppingCartUseCase = container.resolve(
+            UpdateShoppingCartUseCase
+        );
+
+        const shoppingCart = await updateShoppingCartUseCase.execute({
+            id_shoppingCarts,
+            id_products,
+            quantity: qunatityNumber,
+        });
+
+        return response.status(200).json(shoppingCart);
+    }
+}
