5c20560a641767111977194deba5ae66ca003ae8
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-08 21:35:10 -0300

    create provide for save and delete file in localStorage aplication

---
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -0,0 +1,29 @@
+import upload from "@config/uploadConfig";
+import fs from "fs";
+import { resolve } from "path";
+
+import { IStorageProvider } from "../IStorageProvide";
+
+export class LocalStorageProvider implements IStorageProvider {
+    async save(file: string, folder: string): Promise<string> {
+        await fs.promises.rename(
+            // pegar o arquivo no caminho recebido
+            resolve(upload.tmpFolder, file),
+            // colocar o arquivo no caminho destinado
+            resolve(`${upload.tmpFolder}/${folder}`, file)
+        );
+
+        return file;
+    }
+    async delete(file: string, folder: string): Promise<void> {
+        // caminho do arquivo no caminho destinado
+        const fileName = resolve(`${upload.tmpFolder}/${folder}`, file);
+
+        try {
+            await fs.promises.stat(fileName);
+        } catch {
+            return;
+        }
+        await fs.promises.unlink(fileName);
+    }
+}
