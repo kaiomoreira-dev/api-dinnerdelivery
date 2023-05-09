3bdf6f41c0121498a043131938155124aa05716f
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-08 21:29:44 -0300

    create route patch for update photo in product

--- src/shared/infra/http/routes/products.routes.ts
+++ src/shared/infra/http/routes/products.routes.ts
@@ -1,9 +1,12 @@
+import uploadConfig from "@config/uploadConfig";
 import { CreateProductsController } from "@modules/products/useCases/createProduct/CreateProductsController";
+import { CreateProductImageController } from "@modules/products/useCases/createProductImage/CreateProductImageController";
 import { DeleteProductController } from "@modules/products/useCases/deleteProduct/DeleteProductController";
 import { FindProductByIdController } from "@modules/products/useCases/findProductById/FindProductByIdController";
 import { ListProductsController } from "@modules/products/useCases/listProduct/ListProductsController";
 import { UpdateProductController } from "@modules/products/useCases/updateProduct/UpdateProductController";
 import { Router } from "express";
+import multer from "multer";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
@@ -19,6 +22,10 @@ const findProductByIdController = new FindProductByIdController();
 const updateProductController = new UpdateProductController();
 
 const deleteProductController = new DeleteProductController();
+
+const createProductImageController = new CreateProductImageController();
+
+const uploadProduct = multer(uploadConfig);
 
 productRoutes.post(
   "/",
@@ -41,6 +48,14 @@ productRoutes.put(
   ensureAuthenticate,
   ensureAdmin,
   updateProductController.handle
+);
+
+productRoutes.patch(
+  "/import/:id",
+  ensureAuthenticate,
+  ensureAdmin,
+  uploadProduct.single("image"),
+  createProductImageController.handle
 );
 
 productRoutes.delete(
