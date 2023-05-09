9a415fc088b1aa247cb2bb6411e4cb570c8ddfef
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-09 19:28:35 -0300

    create controller for delete product in cart

---
+++ src/modules/shoppingCarts/useCases/deleteProductInCart/DeleteProductInShopingCartController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { DeleteProductInShopingCartUseCase } from "./DeleteProductInShopingCartUseCase";
+
+export class DeleteProductInShopingCartController {
+    async handle(request: Request, response: Response) {
+        const { id_products, id_shoppingCarts } = request.params;
+
+        const deleteProductInShopingCartUseCase = container.resolve(
+            DeleteProductInShopingCartUseCase
+        );
+
+        await deleteProductInShopingCartUseCase.execute({
+            id_products,
+            id_shoppingCarts,
+        });
+
+        return response
+            .status(200)
+            .json({ message: "Product in shoppingCart deleted successfully" });
+    }
+}
