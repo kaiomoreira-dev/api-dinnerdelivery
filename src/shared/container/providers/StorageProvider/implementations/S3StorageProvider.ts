fc18229c8d247a2f303aa71749f19924ee152994
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-15 11:49:04 -0300

    alter repository S3Storage with update SDK-AWS S3

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
+import { S3 } from "@aws-sdk/client-s3";
 import upload from "@config/uploadConfig";
-import { S3 } from "aws-sdk";
 import fs from "fs";
 import mime from "mime";
 import { resolve } from "path";
@@ -23,15 +24,13 @@ export class S3StorageProvider implements IStorageProvider {
 
         const ContentType = mime.getType(filePath);
 
-        await this.client
-            .putObject({
-                Bucket: `${process.env.AWS_BUCKET}/${folder}`,
-                Key: file,
-                ACL: "public-read",
-                Body: readedFilePath,
-                ContentType,
-            })
-            .promise();
+        await this.client.putObject({
+            Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+            Key: file,
+            ACL: "public-read",
+            Body: readedFilePath,
+            ContentType,
+        });
 
         // removendo das pasta tmp
         await fs.promises.unlink(filePath);
@@ -39,11 +38,9 @@ export class S3StorageProvider implements IStorageProvider {
         return file;
     }
     async delete(file: string, folder: string): Promise<void> {
-        await this.client
-            .deleteObject({
-                Bucket: `${process.env.AWS_BUCKET}/${folder}`,
-                Key: file,
-            })
-            .promise();
+        await this.client.deleteObject({
+            Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+            Key: file,
+        });
     }
 }
