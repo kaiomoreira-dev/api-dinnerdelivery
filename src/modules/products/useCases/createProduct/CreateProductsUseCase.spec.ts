53938e520fb4def0e01ef191057165e486e52ff5
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-16 11:50:48 -0300

    alter teste integration when create product remvoeing user verify

--- src/modules/products/useCases/createProduct/CreateProductsUseCase.spec.ts
+++ src/modules/products/useCases/createProduct/CreateProductsUseCase.spec.ts
@@ -2,42 +2,21 @@
 /* eslint-disable import/no-unresolved */
 import { redisClient } from "@config/redisClient";
 import { faker } from "@faker-js/faker";
-import { ICreateUserDTO } from "@modules/accounts/dtos/CreateUserDTO";
-import { RefreshTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/RefreshTokensRepositoryInMemory";
-import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
-import { AuthenticateUserUseCase } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase";
-import { CreateUserUseCase } from "@modules/accounts/useCases/createUser/CreateUserUseCase";
 import { ICreateProductsDTO } from "@modules/products/dtos/ICreateProductsDTO";
 import { ProductsRepositoryInMemory } from "@modules/products/repositories/in-memory/ProductsRepositoryInMemory";
 
-import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 import { CreateProductsUseCase } from "./CreateProductsUseCase";
 
 let productsRepositoryInMemory: ProductsRepositoryInMemory;
 let createProductsUseCase: CreateProductsUseCase;
-let usersRepositoryInMemory: UsersRepositoryInMemory;
-let createUserUseCase: CreateUserUseCase;
-let authenticateUserUseCase: AuthenticateUserUseCase;
-let dayjsDateProvider: DayjsDateProvider;
-let refreshTokensRepositoryInMemory: RefreshTokensRepositoryInMemory;
 
 describe("Create Product UseCase", () => {
     beforeEach(() => {
-        usersRepositoryInMemory = new UsersRepositoryInMemory();
         productsRepositoryInMemory = new ProductsRepositoryInMemory();
-        createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
-        dayjsDateProvider = new DayjsDateProvider();
-        refreshTokensRepositoryInMemory = new RefreshTokensRepositoryInMemory();
-        authenticateUserUseCase = new AuthenticateUserUseCase(
-            usersRepositoryInMemory,
-            refreshTokensRepositoryInMemory,
-            dayjsDateProvider
-        );
         createProductsUseCase = new CreateProductsUseCase(
-            productsRepositoryInMemory,
-            usersRepositoryInMemory
+            productsRepositoryInMemory
         );
     });
 
@@ -46,23 +25,6 @@ describe("Create Product UseCase", () => {
     });
 
     it("should be able to create product", async () => {
-        const user: ICreateUserDTO = {
-            name: faker.name.fullName(),
-            email: faker.internet.email(),
-            password: faker.datatype.string(8),
-            address: faker.address.streetAddress(),
-            admin: true,
-        };
-
-        // Create a user
-        const createdUser = await createUserUseCase.execute(user);
-
-        // Authenticate a user
-        await authenticateUserUseCase.execute({
-            email: user.email,
-            password: user.password,
-        });
-
         const product: ICreateProductsDTO = {
             name: faker.name.fullName(),
             description: faker.commerce.productDescription(),
@@ -70,31 +32,11 @@ describe("Create Product UseCase", () => {
             unit_price: Number(faker.commerce.price()),
         };
 
-        const createProduct = await createProductsUseCase.execute(
-            product,
-            createdUser.id
-        );
+        const createProduct = await createProductsUseCase.execute(product);
         expect(createProduct).toHaveProperty("id");
     });
 
     it("should not be able to create product with name already exists", async () => {
-        const user: ICreateUserDTO = {
-            name: faker.name.fullName(),
-            email: faker.internet.email(),
-            password: faker.datatype.string(8),
-            address: faker.address.streetAddress(),
-            admin: true,
-        };
-
-        // Create a user
-        const createdUser = await createUserUseCase.execute(user);
-
-        // Authenticate a user
-        await authenticateUserUseCase.execute({
-            email: user.email,
-            password: user.password,
-        });
-
         const product: ICreateProductsDTO = {
             name: faker.name.fullName(),
             description: faker.commerce.productDescription(),
@@ -102,42 +44,32 @@ describe("Create Product UseCase", () => {
             unit_price: Number(faker.commerce.price()),
         };
 
-        const createProduct = await createProductsUseCase.execute(
-            product,
-            createdUser.id
-        );
+        const createProduct = await createProductsUseCase.execute(product);
 
         await expect(
-            createProductsUseCase.execute(
-                {
-                    id: faker.datatype.uuid(),
-                    name: createProduct.name,
-                    description: faker.commerce.productDescription(),
-                    quantity: faker.datatype.number(4),
-                    unit_price: Number(faker.commerce.price()),
-                },
-                createdUser.id
-            )
+            createProductsUseCase.execute({
+                id: faker.datatype.uuid(),
+                name: createProduct.name,
+                description: faker.commerce.productDescription(),
+                quantity: faker.datatype.number(4),
+                unit_price: Number(faker.commerce.price()),
+            })
         ).rejects.toEqual(new AppError("Product already exists", 401));
     });
 
+    it("should not be able to create product with name is empty", async () => {
+        await expect(
+            createProductsUseCase.execute({
+                id: faker.datatype.uuid(),
+                name: null,
+                description: faker.commerce.productDescription(),
+                quantity: faker.datatype.number(4),
+                unit_price: Number(faker.commerce.price()),
+            })
+        ).rejects.toEqual(new AppError("Product name is empty", 401));
+    });
+
     it("should not be able to create product with quantity less than or equal to zero", async () => {
-        const user: ICreateUserDTO = {
-            name: faker.name.fullName(),
-            email: faker.internet.email(),
-            password: faker.datatype.string(8),
-            address: faker.address.streetAddress(),
-            admin: true,
-        };
-
-        const createdUser = await createUserUseCase.execute(user);
-
-        // Authenticate a user
-        await authenticateUserUseCase.execute({
-            email: user.email,
-            password: user.password,
-        });
-
         const product: ICreateProductsDTO = {
             name: faker.name.fullName(),
             description: faker.commerce.productDescription(),
@@ -145,29 +77,12 @@ describe("Create Product UseCase", () => {
             unit_price: Number(faker.commerce.price()),
         };
 
-        await expect(
-            createProductsUseCase.execute(product, createdUser.id)
-        ).rejects.toEqual(new AppError("Quantity is not valid", 401));
+        await expect(createProductsUseCase.execute(product)).rejects.toEqual(
+            new AppError("Quantity is not valid", 401)
+        );
     });
 
     it("should not be able to create product with unit_price less than or equal to zero ", async () => {
-        const user: ICreateUserDTO = {
-            name: faker.name.fullName(),
-            email: faker.internet.email(),
-            password: faker.datatype.string(8),
-            address: faker.address.streetAddress(),
-            admin: true,
-        };
-
-        // Create a user
-        const createdUser = await createUserUseCase.execute(user);
-
-        // Authenticate a user
-        await authenticateUserUseCase.execute({
-            email: user.email,
-            password: user.password,
-        });
-
         const product: ICreateProductsDTO = {
             name: faker.name.fullName(),
             description: faker.commerce.productDescription(),
@@ -175,8 +90,8 @@ describe("Create Product UseCase", () => {
             unit_price: 0,
         };
 
-        await expect(
-            createProductsUseCase.execute(product, createdUser.id)
-        ).rejects.toEqual(new AppError("Price unit is not valid", 401));
+        await expect(createProductsUseCase.execute(product)).rejects.toEqual(
+            new AppError("Price unit is not valid", 401)
+        );
     });
 });
