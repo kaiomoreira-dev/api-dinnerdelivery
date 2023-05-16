2822c864f5642192574dd6b51a665efcc8ba798d
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-16 11:45:25 -0300

    alter status code in controler delete product for 204

--- src/modules/products/useCases/deleteProduct/DeleteProductController.ts
+++ src/modules/products/useCases/deleteProduct/DeleteProductController.ts
@@ -6,19 +6,15 @@ import { DeleteProductUseCase } from "./DeleteProductUseCase";
 export class DeleteProductController {
     async handle(request: Request, response: Response) {
         const { id: id_products } = request.params;
-        const { id: id_users } = request.user;
 
         const deleteProductUseCase = container.resolve(DeleteProductUseCase);
 
-        await deleteProductUseCase.execute(
-            {
-                id: id_products,
-            },
-            id_users
-        );
+        await deleteProductUseCase.execute({
+            id: id_products,
+        });
 
         return response
-            .status(200)
+            .status(204)
             .json({ message: "Product deleted successfully" });
     }
 }
