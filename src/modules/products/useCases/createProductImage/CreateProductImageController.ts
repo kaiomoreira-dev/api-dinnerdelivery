803c401d1993915a02858acefe74255012a1e628
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-16 11:46:03 -0300

    alter controller create iamge product ffor verify iamge receive and remove send id_user

--- src/modules/products/useCases/createProductImage/CreateProductImageController.ts
+++ src/modules/products/useCases/createProductImage/CreateProductImageController.ts
@@ -1,26 +1,31 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateProductImageUseCase } from "./CreateProductImageUseCase";
 
 export class CreateProductImageController {
     async handle(request: Request, response: Response) {
-        const { id: id_products } = request.params;
-        const { id: id_users } = request.user;
+        try {
+            const { id: id_products } = request.params;
 
-        const imageName = request.file.filename;
+            const imageName = request.file.filename;
 
-        const createProductImageUseCase = container.resolve(
-            CreateProductImageUseCase
-        );
+            const createProductImageUseCase = container.resolve(
+                CreateProductImageUseCase
+            );
 
-        await createProductImageUseCase.execute(
-            { id: id_products, product_img: imageName },
-            id_users
-        );
+            await createProductImageUseCase.execute({
+                id: id_products,
+                product_img: imageName,
+            });
 
-        return response
-            .status(200)
-            .json({ message: "Product updated successfully" });
+            return response
+                .status(200)
+                .json({ message: "Product updated successfully" });
+        } catch {
+            throw new AppError("Image not found", 404);
+        }
     }
 }
