1fd4525543879a5004cf7680adaf4dc9bb098f17
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-16 11:58:30 -0300

    alter path for rotues refresh token in test unity

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.spec.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.spec.ts
@@ -30,17 +30,19 @@ describe("Refresh token Controller", () => {
             password: faker.internet.password(10),
             address: faker.address.streetAddress(),
         };
-        await request(app).post("/users").send(user);
+        await request(app).post("/api/users").send(user);
 
-        const authenticateUser = await request(app).post("/sessions").send({
+        const authenticateUser = await request(app).post("/api/sessions").send({
             email: user.email,
             password: user.password,
         });
         const { refresh_token } = authenticateUser.body;
 
-        const refreshToken = await request(app).post("/refresh-token").send({
-            token: refresh_token,
-        });
+        const refreshToken = await request(app)
+            .post("/api/refresh-token")
+            .send({
+                token: refresh_token,
+            });
 
         expect(refreshToken.status).toBe(200);
     });
@@ -53,17 +55,19 @@ describe("Refresh token Controller", () => {
             password: faker.internet.password(10),
             address: faker.address.streetAddress(),
         };
-        await request(app).post("/users").send(user);
+        await request(app).post("/api/users").send(user);
 
-        await request(app).post("/sessions").send({
+        await request(app).post("/api/sessions").send({
             email: user.email,
             password: user.password,
         });
 
-        const refreshToken = await request(app).post("/refresh-token").send({
-            token: "invalidtoken",
-        });
+        const refreshToken = await request(app)
+            .post("/api/refresh-token")
+            .send({
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJrZW5zYW50QHRlc3QuY29tIiwiaWF0IjoxNjgzODMzMTA5LCJleHAiOjE2ODQ0Mzc5MDksInN1YiI6IjA5MDdjMDYzLTc3Y2QtNDdkYi1hZmE1LWE0M2JjZTNhY2IzZCJ9.ykN75GR3R0Y0kAehhnP4rkYOXXckQp5jkw7BVRgABmw",
+            });
 
-        expect(refreshToken.status).toBe(401);
+        expect(refreshToken.status).toBe(404);
     });
 });
