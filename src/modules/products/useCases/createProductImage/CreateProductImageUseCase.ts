3e74d8ae8c2b563e96fcabb9a91399c70178bdb7
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-16 11:44:54 -0300

    alter case create image for product return true when finalize and remove verify user id

--- src/modules/products/useCases/createProductImage/CreateProductImageUseCase.ts
+++ src/modules/products/useCases/createProductImage/CreateProductImageUseCase.ts
@@ -1,4 +1,3 @@
-import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { ICreateProductsDTO } from "@modules/products/dtos/ICreateProductsDTO";
 import { IProductsRepository } from "@modules/products/repositories/IProductsRepository";
 import { inject, injectable } from "tsyringe";
@@ -11,40 +10,38 @@ export class CreateProductImageUseCase {
     constructor(
         @inject("ProductsRepository")
         private productsRepository: IProductsRepository,
-        @inject("UsersRepository")
-        private userRepository: IUsersRepository,
         @inject("LocalStorageProvider")
         private localStorageProvider: IStorageProvider
     ) {}
 
-    async execute(
-        { id, product_img }: ICreateProductsDTO,
-        id_users: string
-    ): Promise<void> {
-        const userAuthorized = await this.userRepository.findById(id_users);
-
-        if (!userAuthorized) {
-            throw new AppError("User not authorized to create products", 401);
+    async execute({ id, product_img }: ICreateProductsDTO): Promise<boolean> {
+        if (!id) {
+            throw new AppError("Product is empty", 401);
         }
-
         const productsExists = await this.productsRepository.findById(id);
 
         if (!productsExists) {
             throw new AppError("Product not found", 404);
         }
 
+        if (!product_img) {
+            throw new AppError("Image is empty", 401);
+        }
+
         if (productsExists.product_img) {
             await this.localStorageProvider.delete(
                 productsExists.product_img,
-                "products_img"
+                "products"
             );
         }
 
         await this.localStorageProvider.save(product_img, "products");
 
-        await this.productsRepository.updateById({
+        const productImage = await this.productsRepository.updateById({
             id: productsExists.id,
             product_img,
         });
+
+        return productImage;
     }
 }
