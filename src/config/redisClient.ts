091787eb9b38518fa8d3c35873b9cded947839c8
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-16 20:20:39 -0300

    remove password for connection redis client

--- src/config/redisClient.ts
+++ src/config/redisClient.ts
@@ -4,7 +4,6 @@ import { createClient } from "redis";
 export const redisClient = createClient({
   host: process.env.NODE_ENV === "test" ? "localhost" : process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASS || undefined,
 });
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
