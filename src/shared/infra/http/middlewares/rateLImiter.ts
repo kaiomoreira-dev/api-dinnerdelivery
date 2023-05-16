603b55c8d8cc8a63d06ac106d976e031870c3784
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-16 20:21:37 -0300

    create rateLimiter for protect for atk ddos

---
+++ src/shared/infra/http/middlewares/rateLImiter.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+import { redisClient } from "@config/redisClient";
+import { NextFunction, Request, Response } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+
+import { AppError } from "@shared/errors/AppError";
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "rateLimiter",
+  points: 8, // 10 requests
+  duration: 5, // per 1 second by IP
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError("To many Requests not permited", 429);
+  }
+}
