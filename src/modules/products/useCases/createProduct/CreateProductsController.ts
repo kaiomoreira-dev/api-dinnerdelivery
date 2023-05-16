dc709f99221c1821f8237192729ac89cebde2233
Kaio Moreira <10402971-kaiomoreira-dev@users.noreply.gitlab.com>
2023-05-16 11:51:26 -0300

    alter product controler removing id_user

--- src/modules/products/useCases/createProduct/CreateProductsController.ts
+++ src/modules/products/useCases/createProduct/CreateProductsController.ts
@@ -6,19 +6,15 @@ import { CreateProductsUseCase } from "./CreateProductsUseCase";
 export class CreateProductsController {
     async handle(request: Request, response: Response) {
         const { name, description, unit_price, quantity } = request.body;
-        const { id: id_users } = request.user;
 
         const createProductsUseCase = container.resolve(CreateProductsUseCase);
 
-        const product = await createProductsUseCase.execute(
-            {
-                name,
-                description,
-                unit_price,
-                quantity,
-            },
-            id_users
-        );
+        const product = await createProductsUseCase.execute({
+            name,
+            description,
+            unit_price,
+            quantity,
+        });
 
         return response.status(201).json(product);
     }
