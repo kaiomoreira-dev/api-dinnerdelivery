8a40a8ca17c05022fda5bc5d40d06c8bb621700a
Kaio Moreira <kaio-dev@outlook.com>
2023-08-08 12:42:00 -0300

    update: new version code

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 
 export default {
-  secret_token: process.env.SECRET_TOKEN,
-  expire_in_token: process.env.EXPIRE_IN_TOKEN,
-  secret_refresh_token: process.env.SECRET_REFRESH_TOKEN,
-  expire_refresh_token: process.env.EXPIRE_REFRESH_TOKEN,
+  secret_token: JSON.parse(process.env.SECRET_TOKEN),
+  expire_in_token: JSON.parse(process.env.EXPIRE_IN_TOKEN),
+  secret_refresh_token: JSON.parse(process.env.SECRET_REFRESH_TOKEN),
+  expire_refresh_token: JSON.parse(process.env.EXPIRE_REFRESH_TOKEN),
   days_refresh_token: Number(process.env.DAYS_REFRESH_TOKEN),
 };
